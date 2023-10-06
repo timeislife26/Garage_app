d971edd71f0c4125726b0d833157b27032fd43f9
Jason Fleming <jflemin26@gmail.com>
2023-10-06 22:26:39 +0100

    Moved the Radio buttons to be in the radio group. Add code to check which radio button is selected. Provided a toast message for the user if they have not selected an option when trying to press submit.

--- app/src/main/java/com/example/garage_app/MainActivity.java
+++ app/src/main/java/com/example/garage_app/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.garage_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RadioButton car = findViewById(R.id.carRB);
+        RadioButton bike = findViewById(R.id.bikeRB);
+        RadioButton other = findViewById(R.id.otherRB);
+        Button submit = findViewById(R.id.submitBtn);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int option = 0;
+                option = (car.isChecked())?1:(bike.isChecked())?2:other.isChecked()?3:0;
+                if (option == 0) {
+                    Toast.makeText(getApplicationContext(), R.string.unselected, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
