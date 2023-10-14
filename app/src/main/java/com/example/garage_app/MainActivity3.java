bbf6622d0378225989e55801e89d0510818393a9
Jason Fleming <jflemin26@gmail.com>
2023-10-14 19:02:26 +0100

    Added a FAB to MainActivity3 that will allow the user to email the garage with a prepopulated subject text of the vehicle quote that they want to email about

--- app/src/main/java/com/example/garage_app/MainActivity3.java
+++ app/src/main/java/com/example/garage_app/MainActivity3.java
@@ -2,6 +2,7 @@ package com.example.garage_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,7 +24,9 @@ public class MainActivity3 extends AppCompatActivity {
         ImageView vehicleImage = findViewById(R.id.vehicleImage);
         TextView vehiclePrice = findViewById(R.id.vehiclePrice);
         TextView vehicleDescription = findViewById(R.id.vehicleDescription);
-        FloatingActionButton homeBtn = findViewById(R.id.homeFAB);
+        FloatingActionButton homeFAB = findViewById(R.id.homeFAB);
+        FloatingActionButton emailFAB = findViewById(R.id.emailFAB);
+
 
 
 
@@ -34,7 +37,7 @@ public class MainActivity3 extends AppCompatActivity {
         String[] vehicleList;
 
 
-        homeBtn.setOnClickListener(new View.OnClickListener() {
+        homeFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent home = new Intent(MainActivity3.this, MainActivity.class);
@@ -42,12 +45,15 @@ public class MainActivity3 extends AppCompatActivity {
             }
         });
 
+
+
         if (extras != null) {
             vehiclePic = extras.getInt("vehiclePic");
             vehicleImage.setImageResource(extras.getInt("vehiclePic"));
             //vehicleName = extras.getString("position");
             int position = (int)extras.get("position");
             vehicleType = extras.getInt("vehicleType");
+            String quote = "";
             switch (vehicleType){
                 case 0:
                     vehicleName.setText(getString(R.string.errorCase));
@@ -58,16 +64,19 @@ public class MainActivity3 extends AppCompatActivity {
                     vehicleName.setText(getResources().getStringArray(R.array.cars)[position]);
                     vehiclePrice.setText(getResources().getStringArray(R.array.carPrice)[position]);
                     vehicleDescription.setText(getResources().getStringArray(R.array.carDescription)[position]);
+                    quote = getResources().getStringArray(R.array.carQuote)[position];
                     break;
-                    case 2:
+                case 2:
                     vehicleName.setText(getResources().getStringArray(R.array.bikes)[position]);
                     vehiclePrice.setText(getResources().getStringArray(R.array.bikePrice)[position]);
                     vehicleDescription.setText(getResources().getStringArray(R.array.bikeDescription)[position]);
+                    quote = getResources().getStringArray(R.array.bikeQuote)[position];
                     break;
-                    case 3:
+                case 3:
                     vehicleName.setText(getResources().getStringArray(R.array.other)[position]);
                     vehiclePrice.setText(getResources().getStringArray(R.array.otherPrice)[position]);
                     vehicleDescription.setText(getResources().getStringArray(R.array.otherDescription)[position]);
+                    quote = getResources().getStringArray(R.array.other)[position];
                     break;
             }
 
@@ -78,8 +87,28 @@ public class MainActivity3 extends AppCompatActivity {
             vehicleImage.setImageResource(extras.getInt("vehiclePic"));
             TextView vehiclePrice = findViewById(R.id.vehiclePrice);
             */
+            String finalQuote = quote;
+            emailFAB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    composeEmail("Quote:" + finalQuote);
+                }
+            });
+
+        }
+    }
 
 
+    private void composeEmail(String subject) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"Garage@Garage.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send Email"));
+        } catch (ActivityNotFoundException e) {
+            // Handle if no email app is available
         }
     }
 }
